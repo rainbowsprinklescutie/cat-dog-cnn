950c2ca914f2f6f2e96e1df7ec8c0f8c3f434b4a
binary4oul <araisora30@gmail.com>
2026-06-19 13:17:32 +0800

    define NN

--- main.torch.py
+++ main.torch.py
@@ -1,8 +1,8 @@
 import torch
+from torch import nn
+import torch.nn.functional as F
 from torch.utils.data import DataLoader
-import matplotlib.pyplot as plt
-import pandas as pd
-from keras import Sequential, layers
+import torch.optim as optim
 
 from tensorflow.keras.utils import image_dataset_from_directory
 from init import base_dataset_dir
@@ -25,6 +25,69 @@ test_datagen = image_dataset_from_directory(base_dataset_dir,
 train_loader = DataLoader(train_datagen, batch_size=32, shuffle=True)
 test_loader = DataLoader(test_datagen, batch_size=32, shuffle=False)
 
+# model = Sequential([
+#     layers.Conv2D(32, (3, 3), activation='relu', input_shape=(200, 200, 3)),
+#     layers.MaxPooling2D((2, 2)),
+#     layers.Conv2D(64, (3, 3), activation='relu'),
+#     layers.MaxPooling2D((2, 2)),
+#     layers.Conv2D(128, (3, 3), activation='relu'),
+#     layers.MaxPooling2D((2, 2)),
+#     layers.Flatten(),
+#     layers.Dense(512, activation='relu'),
+#     layers.Dense(1, activation='sigmoid')
+# ])
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # Keras: layers.Conv2D(32, (3, 3), input_shape=(200, 200, 3))
+        self.conv1 = nn.Conv2d(3, 32, 3)
+
+        # Keras: layers.MaxPooling2D((2, 2)) (We use one object for all pools)
+        self.pool = nn.MaxPool2d(2, 2)
+
+        # Keras: layers.Conv2D(64, (3, 3))
+        self.conv2 = nn.Conv2d(32, 64, 3)
+
+        # Keras: layers.Conv2D(128, (3, 3))
+        self.conv3 = nn.Conv2d(64, 128, 3)
+
+        # Keras: layers.Flatten() -> maps to 128 * 23 * 23 = 67712
+        # Keras: layers.Dense(512)
+        self.fc1 = nn.Linear(128 * 23 * 23, 512)
+
+        # Keras: layers.Dense(1)
+        self.fc2 = nn.Linear(512, 1)
+
+    def forward(self, x):
+        # Layer 1: Conv -> Relu -> Pool
+        x = self.pool(F.relu(self.conv1(x)))
+
+        # Layer 2: Conv -> Relu -> Pool
+        x = self.pool(F.relu(self.conv2(x)))
+
+        # Layer 3: Conv -> Relu -> Pool
+        x = self.pool(F.relu(self.conv3(x)))
+
+        # Layer 4: Flatten
+        x = torch.flatten(x, 1)
+
+        # Layer 5: Dense(512) -> Relu
+        x = F.relu(self.fc1(x))
+
+        # Layer 6: Dense(1) -> Sigmoid
+        x = torch.sigmoid(self.fc2(x))
+
+        return x
+
+# Setup Model, loss, and optimizer
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = Net().to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
 # class_names = train_datagen.class_names
 # print(f"class_names: {class_names}")
 #
@@ -40,17 +103,7 @@ test_loader = DataLoader(test_datagen, batch_size=32, shuffle=False)
 #
 # # Model Architecture
 #
-# model = Sequential([
-#     layers.Conv2D(32, (3, 3), activation='relu', input_shape=(200, 200, 3)),
-#     layers.MaxPooling2D((2, 2)),
-#     layers.Conv2D(64, (3, 3), activation='relu'),
-#     layers.MaxPooling2D((2, 2)),
-#     layers.Conv2D(128, (3, 3), activation='relu'),
-#     layers.MaxPooling2D((2, 2)),
-#     layers.Flatten(),
-#     layers.Dense(512, activation='relu'),
-#     layers.Dense(1, activation='sigmoid')
-# ])
+
 #
 # model.summary()
 #
